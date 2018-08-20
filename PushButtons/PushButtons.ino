0b485bb5026d757abb049603135f31e062d4502c
Chris <vector8@users.noreply.github.com>
2018-08-20 19:28:43 -0400

    Added PushButtons.

---
+++ PushButtons/PushButtons.ino
@@ -0,0 +1,36 @@
+const int leftPin = 2;
+const int rightPin = 4;
+
+int leftState = 0;
+int rightState = 0;
+
+void setup() 
+{
+  Serial.begin(9600);
+
+  pinMode(leftPin, INPUT);
+  pinMode(rightPin, INPUT);
+}
+
+void loop() 
+{
+  leftState = digitalRead(leftPin);
+  rightState = digitalRead(rightPin);
+
+  int output = 0;
+
+  if(rightState == HIGH)
+  {
+    output += 1;
+  }
+  
+  if(leftState == HIGH)
+  {
+    output += 2;
+  }
+
+  Serial.println(String(output));
+  
+  delay(100);
+}
+
